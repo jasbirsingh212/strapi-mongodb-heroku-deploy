bd97b3578ff4910226675903516acbcd42b3b66d
jasbirsingh212 <71191894+jasbirsingh212@users.noreply.github.com>
2021-12-17 21:43:43 +0530

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd3b96d35e5abfc2236b9b80c9b632c21'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e93705b8-1233-4b24-a6a9-6f4b1ab7d30f'
+};
