bc828c98d9563e93f8c61c42132a7f028f889c6c
jasbirsingh212 <71191894+jasbirsingh212@users.noreply.github.com>
2021-12-29 15:50:01 +0530

    added job content type

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1338),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'd3b96d35e5abfc2236b9b80c9b632c21'),
